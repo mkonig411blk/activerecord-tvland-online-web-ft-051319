7b3c3c84bda41f2f33a381aeaff7d22792c3acfe
Margot Konig <margot.konig@blackrock.com>
2019-06-20 22:10:18 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    self.first_name + " " + self.last_name
+  end 
+  
+  def list_roles 
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end 
+  
+   # TODO create a list_roles method
+    # TODO: build a method on actor that will return an array of
+    # strings in the form "#{character_name} - #{show_name}"
 end
