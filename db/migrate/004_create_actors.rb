1d6238aa901b4fb78622df6bd16e30b4195886d6
Margot Konig <margot.konig@blackrock.com>
2019-06-20 22:03:22 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,6 +3,7 @@ class CreateActors < ActiveRecord::Migration[5.1]
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
+      t.string :character_id
     end
   end
 end
